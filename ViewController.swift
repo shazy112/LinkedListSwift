a2a987d2486ee8fee11cd1eef97b16c874fe901c
Shiraz Ahmed Khan <shiraz.khan@aymcommerce.com>
2020-08-06 21:39:57 +0500

    Complete Linkedlist

--- ViewController.swift
+++ ViewController.swift
@@ -36,6 +36,37 @@ class Linkedlist {
         }
     }
     
+    func addNodeAtIndex(index:Int, value:Int) {
+        let node = Node(value: value)
+        var previous = head
+        if index == 0 {
+            previous?.next = head
+            head = previous
+        }
+        else {
+            for i in 0..<index - 1 {
+                previous = previous?.next
+            }
+            node.next = previous?.next
+            previous?.next = node
+        }
+    }
+    
+    func deleteNodeAtIndex(index:Int) {
+               var previous = head
+                var next = head
+               if index == 0 {
+                head = previous?.next
+               }
+               else {
+                for _ in 0..<index-1 {
+                    previous = previous?.next
+                }
+                next = previous?.next
+                previous?.next = next?.next
+        }
+    }
+    
     func showNodes() {
         var current = head
         print(current?.value)
@@ -49,5 +80,12 @@ class Linkedlist {
 var linklist = Linkedlist()
 linklist.addNodeAtEnd(value: 5)
 linklist.addNodeAtEnd(value: 15)
-linklist.addNodeAtTheStart(value: 3)
+linklist.addNodeAtEnd(value: 22)
+linklist.addNodeAtEnd(value: 33)
+linklist.addNodeAtEnd(value: 44)
+
+//linklist.addNodeAtTheStart(value: 3)
+//linklist.addNodeAtIndex(index: 1, value: 33)
+
+linklist.deleteNodeAtIndex(index: 2)
 linklist.showNodes()
